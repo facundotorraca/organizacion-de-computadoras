3b9e29590bae520e5b79b3c42a3ba8a23e884a3b
nicofandos <nfandos@fi.uba.ar>
2020-05-12 23:26:44 -0300

    Mergesort.h calloc

--- trabajo-practico-1/mergesort.h
+++ trabajo-practico-1/mergesort.h
@@ -1,75 +1,74 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 
+#include <stdlib.h>
 #include <math.h>
 
-/*--------------------ESTO VA EN MIPS-------------------*/
-// Shift left one position starting from pos
-// The (p) array is modified in place
-void _shift_left(int p[], int pos, int start) {
-    while (pos != start) {
-        p[pos] = p[pos - 1];
-        pos--;
-    }
+// Merges two subarrays of arr[]. The subarray is arr[l..m]
+// with the subarray is arr[m+1..r]. Receives an auxiliary
+// array to do the merge (t). It is guaranteed that (t)
+// has enougth size to store the entire array.
+void _merge(int p[], int t[], int l, int m, int r) {
+	int l_size = m - l + 1;    // left subarray size
+	int r_size = r - m;        // right subarray size
+
+    int i = 0; // Index left subarray
+    int j = 0; // Index right subarray
+    int k = l; // Index merge subarray
+
+	while (i < l_size && j < r_size) {
+		if (p[l + i] <= p[m + 1 + j]) {
+			t[k] = p[l + i];
+			i++;
+		} else {
+			t[k] = p[m + 1 + j];
+			j++;
+		}
+		k++;
+	}
+
+	// Copy the remaining elements of left side, if there are any
+	while (i < l_size) {
+		t[k] = p[l + i];
+		i++;
+		k++;
+	}
+
+	// Copy the remaining elements of right side, if there are any
+	while (j < r_size) {
+	    t[k] = p[m + 1 + j];
+		j++;
+		k++;
+	}
+
+    // Copy auxiliary elements on the original array
+    for (i = l; i < k; i++)
+        p[i] = t[i];
 }
 
-// Merges two subarrays of P[] in place.
-// The subarray is P[l..m] with the
-// subarray p[m+1..r].
-void _merge(int p[], int l, int m, int r) {
-
-    // if the direct merge is already sorted
-    if (p[m] <= p[m + 1])
-        return;
-
-    // Two pointers to maintain start
-    // of both arrays to merge
-    int i = l;
-    int j = m + 1;
-
-    while (i <= m && j <= r) {
-
-        if (p[i] > p[j]) {
-            // Save value before shifting
-            int value = p[j];
-
-            // shift all the elements between element
-            // p[i] and element p[i], right by 1.
-            _shift_left(p, j, i);
-            p[i] = value;
-
-            // update all the pointers
-            i++; m++; j++;
-        } else {
-            i++;
-        }
-    }
-}
-
-// Sort a subarray of p array recursively.
-// The subarray is p[l..r]. The array is
-// modified in place. (l) is the left index,
-// (r) is the right index.
-void _merge_sort_rec(int p[], int l, int r) {
+void _merge_sort_rec(int p[], int t[], int l, int r) {
 	if (l < r) {
 		//middle of the vector (p)
 		int m = l+(r-l)/2;
 
-		_merge_sort_rec(p, l, m);
-		_merge_sort_rec(p, m+1, r);
+		_merge_sort_rec(p, t, l, m);
+		_merge_sort_rec(p, t, m+1, r);
 
-		_merge(p, l, m, r);
+		_merge(p, t, l, m, r);
 	}
 }
-/*------------------------------------------------------*/
 
 // Receives a pointer to the array to be sorted (p)
 // and an integer with the size of the array (size).
 // Sorts the array modifing the original one.
 void merge_sort(int p[], int size) {
-    // a0 a1 a2 a3
-    // Replace by Assembler Function merge_sort_MIPS(p, size);
-    _merge_sort_rec(p, 0, size - 1);
+    // auxiliary array used for merge
+    int* t = calloc(size, sizeof(int));
+
+    _merge_sort_rec(p, t, 0, size - 1);
+
+    // free auxiliary array
+    free(t);
 }
 
 #endif
