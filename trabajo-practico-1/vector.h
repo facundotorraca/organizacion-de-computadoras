c7040f48e11236455a2e988f423c931283a39944
facutorraca <ftorraca@gmail.com>
2020-06-03 00:22:22 -0300

    this is not C++

--- trabajo-practico-1/vector.h
+++ trabajo-practico-1/vector.h
@@ -28,7 +28,7 @@ bool vector_empty(vector_t* self) {
 }
 
 void vector_push(vector_t* self, int n) {
-    if (self->size == this->max_size - 1)
+    if (self->size == self->max_size - 1)
         realloc(self->array, self->max_size * 2);
 
     self->array[self->size] = n;
